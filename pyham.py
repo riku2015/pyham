ab94c2c159a7c7dfb09fd308521329218582a993
japekki <japek@titanix.net>
2018-01-14 20:59:25 +0200

    GUI update. Sound test. Deleted unneeded file.

--- pyham.py
+++ pyham.py
@@ -4,44 +4,59 @@
 programName = "pyham"
 programVersion = "0.001"
 
-#import wxversion
-#wxversion.select("2.8")
 import wx, wx.html
 import sys
+#import sounddevice as sd
+import wave
+import pyaudio
 
-from window import MyFrame1
+from window import FrameMain
 
-# OLD TEST CODE:
-	# from tests import HtmlWindow
-	# from tests import AboutBox
-	# from tests import Frame
+class Mainwindow(FrameMain):
+	#constructor
+	def __init__(self,parent):
+		#initialize parent class
+		FrameMain.__init__(self,parent)
+	def click_ptt(self,event):
+		try:
+			print >>sys.stderr, "PTT button clicked."
+			# Play sound:
+			CHUNK = 1024
+			wf = wave.open("sound.wav", 'rb')
+			p = pyaudio.PyAudio()
+			stream = p.open(format=p.get_format_from_width(wf.getsampwidth()), channels=wf.getnchannels(), rate=wf.getframerate(), output=True)
+			data = wf.readframes(CHUNK)
+			while data != '':
+				stream.write(data)
+				data = wf.readframes(CHUNK)
+			stream.stop_stream()
+			stream.close()
+			p.terminate()
 
-class MainFrame(MyFrame1):
-    #constructor
-    def __init__(self,parent):
-        #initialize parent class
-        MyFrame1.__init__(self,parent)
- 
-    def ptt_push(self,event):
-        try:
-			print >>sys.stderr, "ptt painettu"
-        except Exception:
-            print 'error'
+		except Exception:
+			print 'error'
 
-	
-app = wx.App(False)   # Error messages go to popup window
-main = MainFrame(None)
-main.Show()
+	def click_connect(self,event):
+		print >>sys.stderr, "Connect to server."
+
+	def click_disconnect(self,event):
+		print >>sys.stderr, "Disconnect from server."
+
+# Create wxwidgets application:
+app = wx.App(False)
+mainwindow = Mainwindow(parent=None)
+
+# Show main window:
+mainwindow.Show()
+
+# Execute GUI:
 app.MainLoop()
 
+
 # TODO:
-# - Button to talk (ptt)
 # - File recorder/player (.wav, .mp3 etc.)
 # - Networking
-# - Room/Channel selection
-# - Checkbox for Auto reconnect
-# - Textfields for Server, Port and setting presets.
-# - Mic and speaker volume button, VU meters, scope graph, spectrogram
+# - Scope graph, spectrogram
 # - VoIP protocol
 # - eQSO protocol
 # - Echolink protocol
--- window.py
+++ window.py
@@ -11,15 +11,15 @@ import wx
 import wx.xrc
 
 ###########################################################################
-## Class MyFrame1
+## Class FrameMain
 ###########################################################################
 
-class MyFrame1 ( wx.Frame ):
+class FrameMain ( wx.Frame ):
 	
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"pyham", pos = wx.DefaultPosition, size = wx.Size( 702,509 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		#self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		
 		gSizer3 = wx.GridSizer( 3, 1, 0, 0 )
 		
@@ -45,15 +45,15 @@ class MyFrame1 ( wx.Frame ):
 		self.m_staticText1.Wrap( -1 )
 		gSizer1.Add( self.m_staticText1, 0, wx.ALL, 5 )
 		
-		self.m_textCtrl1 = wx.TextCtrl( self, wx.ID_ANY, u"eqso.titanix.net", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_textCtrl1, 0, wx.ALL|wx.EXPAND, 5 )
+		self.m_textCtrlServer = wx.TextCtrl( self, wx.ID_ANY, u"eqso.titanix.net", wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.m_textCtrlServer, 0, wx.ALL|wx.EXPAND, 5 )
 		
 		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Port", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText2.Wrap( -1 )
 		gSizer1.Add( self.m_staticText2, 0, wx.ALL, 5 )
 		
-		self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, u"5000", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_textCtrl2, 0, wx.ALL|wx.EXPAND, 5 )
+		self.m_textCtrlPort = wx.TextCtrl( self, wx.ID_ANY, u"5000", wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.m_textCtrlPort, 0, wx.ALL|wx.EXPAND, 5 )
 		
 		self.m_buttonConnect = wx.Button( self, wx.ID_ANY, u"Connect", wx.DefaultPosition, wx.DefaultSize, 0 )
 		gSizer1.Add( self.m_buttonConnect, 0, wx.ALL, 5 )
@@ -63,8 +63,8 @@ class MyFrame1 ( wx.Frame ):
 		
 		gSizer1.Add( self.m_buttonDisconnect, 0, wx.ALL, 5 )
 		
-		self.m_checkBox1 = wx.CheckBox( self, wx.ID_ANY, u"Reconnect", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_checkBox1, 0, wx.ALL, 5 )
+		self.m_checkBoxReconnect = wx.CheckBox( self, wx.ID_ANY, u"Reconnect", wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.m_checkBoxReconnect, 0, wx.ALL, 5 )
 		
 		
 		gSizer2.Add( gSizer1, 1, wx.EXPAND, 5 )
@@ -80,27 +80,27 @@ class MyFrame1 ( wx.Frame ):
 		
 		gSizer7 = wx.GridSizer( 3, 2, 0, 0 )
 		
-		self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, u"Speaker", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText8.Wrap( -1 )
-		gSizer7.Add( self.m_staticText8, 0, wx.ALL, 5 )
+		self.m_staticTextSpeaker = wx.StaticText( self, wx.ID_ANY, u"Speaker", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticTextSpeaker.Wrap( -1 )
+		gSizer7.Add( self.m_staticTextSpeaker, 0, wx.ALL, 5 )
 		
-		self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, u"Mic", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText9.Wrap( -1 )
-		gSizer7.Add( self.m_staticText9, 0, wx.ALL, 5 )
+		self.m_staticTextMic = wx.StaticText( self, wx.ID_ANY, u"Mic", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticTextMic.Wrap( -1 )
+		gSizer7.Add( self.m_staticTextMic, 0, wx.ALL, 5 )
 		
-		self.m_gauge11 = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL )
-		self.m_gauge11.SetValue( 50 ) 
-		gSizer7.Add( self.m_gauge11, 0, wx.ALL|wx.EXPAND, 5 )
+		self.m_gaugeSpeaker = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL )
+		self.m_gaugeSpeaker.SetValue( 50 ) 
+		gSizer7.Add( self.m_gaugeSpeaker, 0, wx.ALL|wx.EXPAND, 5 )
 		
-		self.m_gauge1 = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL )
-		self.m_gauge1.SetValue( 50 ) 
-		gSizer7.Add( self.m_gauge1, 0, wx.ALL|wx.EXPAND, 5 )
+		self.m_gaugeMic = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL )
+		self.m_gaugeMic.SetValue( 50 ) 
+		gSizer7.Add( self.m_gaugeMic, 0, wx.ALL|wx.EXPAND, 5 )
 		
-		self.m_slider1 = wx.Slider( self, wx.ID_ANY, 50, 0, 100, wx.DefaultPosition, wx.Size( -1,-1 ), wx.SL_HORIZONTAL )
-		gSizer7.Add( self.m_slider1, 0, wx.ALL|wx.EXPAND, 5 )
+		self.m_sliderSpeaker = wx.Slider( self, wx.ID_ANY, 50, 0, 100, wx.DefaultPosition, wx.Size( -1,-1 ), wx.SL_HORIZONTAL )
+		gSizer7.Add( self.m_sliderSpeaker, 0, wx.ALL|wx.EXPAND, 5 )
 		
-		self.m_slider2 = wx.Slider( self, wx.ID_ANY, 50, 0, 100, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
-		gSizer7.Add( self.m_slider2, 0, wx.ALL|wx.EXPAND, 5 )
+		self.m_sliderMic = wx.Slider( self, wx.ID_ANY, 50, 0, 100, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		gSizer7.Add( self.m_sliderMic, 0, wx.ALL|wx.EXPAND, 5 )
 		
 		
 		gSizerVU.Add( gSizer7, 1, wx.EXPAND, 5 )
@@ -147,18 +147,22 @@ class MyFrame1 ( wx.Frame ):
 		self.Centre( wx.BOTH )
 		
 		# Connect Events
-		self.m_buttonConnect.Bind( wx.EVT_BUTTON, self.buttonConnectClicked )
-		self.m_buttonPTT.Bind( wx.EVT_BUTTON, self.ptt_push )
+		self.m_buttonConnect.Bind( wx.EVT_BUTTON, self.click_connect )
+		self.m_buttonDisconnect.Bind( wx.EVT_BUTTON, self.click_disconnect )
+		self.m_buttonPTT.Bind( wx.EVT_BUTTON, self.click_ptt )
 	
 	def __del__( self ):
 		pass
 	
 	
 	# Virtual event handlers, overide them in your derived class
-	def buttonConnectClicked( self, event ):
+	def click_connect( self, event ):
 		event.Skip()
 	
-	def ptt_push( self, event ):
+	def click_disconnect( self, event ):
+		event.Skip()
+	
+	def click_ptt( self, event ):
 		event.Skip()
 	
 
